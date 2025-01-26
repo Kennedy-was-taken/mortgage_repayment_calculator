c3c6e1e0830afbdc098efa28364a2cdeb610da95
Kennedy <tuks1032@gmail.com>
2025-01-26 22:30:49 +0200

    right side page complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MobileComponent } from "./mobile/mobile.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MobileComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css', './styles/media.css']
 })
 export class AppComponent {
-  title = 'mortgage_repayment_calculator';
+  title = 'Mortgage Repayment Calculator';
 }
---
+++ src/app/mobile/mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile',
+  imports: [],
+  templateUrl: './mobile.component.html',
+  styleUrls: ['./mobile.component.css', './styles/media.css']
+})
+export class MobileComponent {
+
+}
