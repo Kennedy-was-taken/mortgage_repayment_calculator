80dc28f8df6d18ccc98f9ed4c44aca4dcbdf7580
Kennedy <tuks1032@gmail.com>
2025-01-30 15:45:43 +0200

    tablet, mobile portrait right side page complete

--- src/app/rightside/rightside.component.ts
+++ src/app/rightside/rightside.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-rightside',
-  imports: [],
+  imports: [ NgIf],
   templateUrl: './rightside.component.html',
   styleUrls: ['./rightside.component.css', './styles/landscape/mobileMedia.css', './styles/portrait/tabletMedia.css', './styles/landscape/tabletMedia.css', './styles/landscape/desktopMedia.css']
 })
 export class RightsideComponent {
 
+  firstScreen = false;
+  secondScreen = true;
 }
